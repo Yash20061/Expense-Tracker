c2d8805b3df7b71c5ae68d70a53ad9c1f20bd395
TaherLakdawala910 <taherlakdawala2006@gmail.com>
2025-04-11 21:17:29 +0530

    Made HTML CSS and JS files

---
+++ frontEnd/Login.js
@@ -0,0 +1,24 @@
+let Def_userName = "ABCD";
+let Def_pass = "1234";
+
+
+let user_name = document.querySelector('.js-user_input').value;
+let user_pass = document.querySelector('.js-user_pass').value;
+
+
+
+
+
+if(user_name === Def_userName)
+{
+  if(pass == Def_pass);
+  {
+    window.location.href = "Home_Page.html";
+  }
+
+}
+else
+{
+  alert("User Not Found!");
+}
+
