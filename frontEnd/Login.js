e89488a5a015f6e2866f8b5647ae485ff2d9b904
Yash <sutharyash4311@gmail.com>
2025-04-11 23:50:50 +0530

    Stack Before Pull

--- frontEnd/Login.js
+++ frontEnd/Login.js
@@ -1,24 +1,25 @@
 let Def_userName = "ABCD";
+
 let Def_pass = "1234";
 
 
-let user_name = document.querySelector('.js-user_input').value;
-let user_pass = document.querySelector('.js-user_pass').value;
+let Login = document.querySelector('.Login_butt');
 
 
 
+Login.addEventListener("click", () => {
 
+  alert("Done");
+  let user_name = document.querySelector('.js-user_input').value;
+  let user_pass = document.querySelector('.js-user_pass').value;
 
-if(user_name === Def_userName)
-{
-  if(pass == Def_pass);
-  {
-    window.location.href = "Home_Page.html";
+  if (user_name === Def_userName) {
+    if (user_pass === Def_pass) {
+      window.location.href = "Home_Page.html";
+    } else {
+      alert("Incorrect Password!");
+    }
+  } else {
+    alert("User Not Found!");
   }
-
-}
-else
-{
-  alert("User Not Found!");
-}
-
+});
